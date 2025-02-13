0cd437c59685a8e9ff885b577558712e30931e65
Eric Ogie Aghahowa <ericogia@yahoo.com>
2025-02-13 20:34:00 +0000

    feat: add color scheme

---
+++ colors.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Define color functions
+blue() { printf "\e[34m%s\e[0m\n" "$1"; }
+green() { printf "\e[32m%s\e[0m\n" "$1"; }
+red() { printf "\e[31m%s\e[0m\n" "$1"; }
+yellow() { printf "\e[33m%s\e[0m\n" "$1"; }
+info() { printf "\e[34m%s\e[0m\n" "$1"; }
+
